c6585eccc7c25b546981e2641bcea01e01bc424f
ardiantrik <ardiantrik@gmail.com>
2023-03-09 16:11:11 +0700

    init LSB seko mat

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-phpinfo();
-#iki apaan?
+
+// Create an image instance
+$im = imagecreatefrompng(
+'https://media.geeksforgeeks.org/wp-content/uploads/geeksforgeeks-13.png');
+
+// Save the image as image1.png
+imagepng($im, 'image1.png');
+
+// // Save the image as image2.png with all filters to disable size compression
+// imagepng($im, 'image2.png', null, PNG_ALL_FILTERS);
+
+// imagedestroy($im);
 ?>
