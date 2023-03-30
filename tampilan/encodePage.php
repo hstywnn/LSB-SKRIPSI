38beca1256b622a4a8a078106a2f4cdb1be58d80
ardiantrik <ardiantrik@gmail.com>
2023-03-30 23:32:14 +0700

    tambah psnr ro ngrapike codingan sitik

--- tampilan/encodePage.php
+++ tampilan/encodePage.php
@@ -10,18 +10,18 @@
             <form action="../encode.php" method="post" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label for="form-gambar" class="form-label">Pilih File:</label>
-                    <input class="form-control" type="file" name="form_gambar" id="form-gambar" multiple>
+                    <input class="form-control" type="file" name="form_gambar" id="form-gambar" multiple required>
                 </div>
                 <div class="mb-3">
                     <label for="form-PIN" class="form-label">Pin rahasia pesan</label>
-                    <input type="number" class="form-control" name="form_PIN" id="form-PIN" placeholder="pin mu">
+                    <input type="number" class="form-control" name="form_PIN" id="form-PIN" placeholder="pin mu" required>
                 </div>
                 <div class="mb-3">
                     <label for="form-pesan" class="form-label">Pesan yang akan disembunyikan</label>
-                    <textarea class="form-control" name="form_pesan" id="form-pesan" rows="3"></textarea>
+                    <textarea class="form-control" name="form_pesan" id="form-pesan" rows="3" required></textarea>
                 </div>
 
-                <input type="submit" class="btn btn-primary" value="submit">Submit</input>
+                <input type="submit" class="btn btn-primary" value="Submit"></input>
             </form>
         </div>
     </div>
